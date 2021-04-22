7892a5ff712f09451b584985fe7960fbbaf1fb2e
Arthur Lima <arthur.lima@zrp.com.br>
2021-04-22 20:28:44 -0300

    day 2

---
+++ src/repositories/SettingsRepository.ts
@@ -0,0 +1,7 @@
+import { EntityRepository, Repository } from 'typeorm';
+import { Settings } from '../models/Settings';
+
+@EntityRepository(Settings)
+class SettingsRepository extends Repository<Settings> {}
+
+export { SettingsRepository };
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,18 @@
 import express from "express";
 
+import "./database";
+
+import { routes } from "./routes";
+
 const app = express();
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello World!" })
-})
+});
+
+app.use(express.json());
+app.use(routes);
 
 app.listen(3019, () => {
   console.log("Application Started");
-});
+}); 
